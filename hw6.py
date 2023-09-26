368ab95ae8c688fe105020611ad73e49c81787fd
Aidanapulpi <danapulpi@gmail.com>
2023-09-26 16:57:35 +0600

    test

---
+++ hw6.py
@@ -0,0 +1,12 @@
+import sqlite3
+db3= sqlite3.connect ('test.db3')
+c=db3.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS users(
+name TEXT,
+age INTEGER,
+password TEXT )''')
+c.execute('''INSERT INTO users VALUES('SHAHA', 23, 'sad2')''')
+
+db3.commit()
+db3.close()
